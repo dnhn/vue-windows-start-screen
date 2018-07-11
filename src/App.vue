1b88af5c18cab2f463631a69e36cbf5cbd7161fa
Nhan Doan <nhandev88@gmail.com>
2018-07-11 19:19:54 +0700

    Change HelloWorld to Start

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <main id="app">
     <router-view/>
-  </div>
+  </main>
 </template>
 
 <script>
@@ -13,11 +12,5 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
