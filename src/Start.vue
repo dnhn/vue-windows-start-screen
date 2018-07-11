3cc2deb8cacc2d1edf4704c080d04b490aa0f10e
Nhan Doan <nhandev88@gmail.com>
2018-07-12 00:24:26 +0700

    Change App to Start

---
+++ src/Start.vue
@@ -0,0 +1,17 @@
+<template>
+  <main id="app">
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'start',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Start from './Start.vue'
+
+import './assets/main.scss'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: h => h(Start)
+});
